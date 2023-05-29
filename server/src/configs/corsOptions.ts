5fa413bdc4c46c288faecedcb81617e4d624d74a
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-05-29 18:10:38 -0300

    fix: trying to fix CORS error

--- server/src/configs/corsOptions.ts
+++ server/src/configs/corsOptions.ts
@@ -5,7 +5,7 @@ console.log(allowedOrigins);
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     console.log(origin);
-    if (origin && allowedOrigins.indexOf(origin) !== -1) {
+    if (origin && origin === "https://duosy.vercel.app") {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
