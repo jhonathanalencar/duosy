f39824cb6fd52bd200fa14a321d74bab072c155a
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-05-29 16:40:49 -0300

    feat: add endpoint to get twitch token

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,30 +1,71 @@
-import express from 'express';
-import axios from 'axios';
-import { z } from 'zod';
-import { prisma } from '../lib/prisma';
+import express from "express";
+import axios from "axios";
+import { z } from "zod";
 
 export const authRouter = express.Router();
 
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const AUTHORIZATION_SERVER_TOKEN_URL = process.env.AUTHORIZATION_SERVER_TOKEN_URL;
+const CLIENT_ID = process.env.CLIENT_ID;
+const AUTHORIZATION_SERVER_TOKEN_URL =
+  process.env.AUTHORIZATION_SERVER_TOKEN_URL;
 
-authRouter.post('/token', async (request, response) =>{
+authRouter.post("/token", async (request, response) => {
   const getAccessTokenQuery = z.object({
     code: z.string(),
     client_id: z.string(),
     redirect_uri: z.string(),
   });
 
-  const { code, client_id, redirect_uri } = getAccessTokenQuery.parse(request.query);
+  const { code, client_id, redirect_uri } = getAccessTokenQuery.parse(
+    request.query
+  );
 
-  try{
-    const data = await axios.post(`${AUTHORIZATION_SERVER_TOKEN_URL}?client_id=${client_id}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}`);
-   
+  try {
+    const data = await axios.post(
+      `${AUTHORIZATION_SERVER_TOKEN_URL}?client_id=${client_id}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_uri}`
+    );
+
     response.status(200).json(data.data);
-  }catch(err){
+  } catch (err) {
     return response.status(500).json({
-      "message": "Something went wrong.",
+      message: "Something went wrong.",
     });
   }
+});
 
-})
+authRouter.get("/auth/authorize", async (request, response) => {
+  try {
+    const accessTokenResponse = await axios.post(
+      "https://id.twitch.tv/oauth2/token",
+      null,
+      {
+        params: {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          grant_type: "client_credentials",
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const { access_token } = accessTokenResponse.data;
+
+    response.status(200).json({ access_token });
+  } catch (error) {
+    console.error(error);
+    const statusCode = 500;
+    const message = "Something went wrong.";
+
+    if (error instanceof axios.AxiosError) {
+      return response
+        .status(error.response?.status ?? statusCode)
+        .json(error.response?.data ?? { error: message });
+    }
+
+    return response.status(statusCode).json({
+      error: message,
+    });
+  }
+});
--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,31 +1,31 @@
-import express from 'express';
-import { z } from 'zod';
+import express from "express";
+import { z } from "zod";
 
-import { prisma } from '../lib/prisma';
+import { prisma } from "../lib/prisma";
 
 export const gameRouter = express.Router();
 
-gameRouter.get('/games', async (request, response) =>{
-  try{
+gameRouter.get("/games", async (request, response) => {
+  try {
     const games = await prisma.game.findMany({
       include: {
         _count: {
           select: {
             ads: true,
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
     return response.status(200).json(games);
-  }catch(error){
+  } catch (error) {
     return response.status(404).json({
-      "message": "No games found.",
+      message: "No games found.",
     });
   }
 });
 
-gameRouter.post('/games', async (request, response) =>{
+gameRouter.post("/games", async (request, response) => {
   const createGameBody = z.object({
     id: z.string(),
     title: z.string(),
@@ -33,44 +33,46 @@ gameRouter.post('/games', async (request, response) =>{
   });
 
   const body = createGameBody.parse(request.body);
-  
-  try{
+
+  try {
     const existingGame = await prisma.game.findUnique({
       where: {
         id: body.id,
-      }
+      },
     });
 
-    if(existingGame){
+    if (existingGame) {
       return response.status(409).json({
-        "message": "This game is already registered!",
+        message: "This game is already registered!",
       });
     }
 
     const game = await prisma.game.create({
-      data:{
+      data: {
         id: body.id,
         title: body.title,
         boxArtUrl: body.boxArtUrl,
-      }
+      },
     });
 
     return response.status(201).json(game);
-  }catch(error){
-    if(error instanceof z.ZodError){
-      if(error.issues[0].message === 'Required'){
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof z.ZodError) {
+      if (error.issues[0].message === "Required") {
         return response.status(400).json({
-          "message": `Please provide ${error.issues[0].path[0]}.`,
+          message: `Please provide ${error.issues[0].path[0]}.`,
         });
       }
 
       return response.status(400).json({
-        "message": `Invalid value for ${error.issues[0].path[0]}.`,
+        message: `Invalid value for ${error.issues[0].path[0]}.`,
       });
     }
-    
+
     return response.status(500).json({
-      "message": "The operation could not be completed.",
+      message: "The operation could not be completed.",
     });
   }
 });
