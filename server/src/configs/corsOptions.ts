517fc223c1a5acb8472bad3c90956160f25f73e9
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-05-29 19:25:30 -0300

    fix: trying to fix CORS error

--- server/src/configs/corsOptions.ts
+++ server/src/configs/corsOptions.ts
@@ -4,7 +4,6 @@ import { allowedOrigins } from "./allowedOrigins";
 
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    console.log(origin, "here");
     if (origin && allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import { corsOptions } from "./configs/corsOptions";
 
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use(authRouter);
